d5e42a16428e60183d35d5cc3a9aa3c8c5c7868e
Jose Estrada <j.estrada49@icloud.com>
2021-06-13 00:02:02 -0700

    migrated from node-sass to sass library

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import '../styles/App.scss';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>25 + 5 Clock</h1>
     </div>
   );
 }
-
-export default App;
