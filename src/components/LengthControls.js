82c2371b3891d8358e4928a012eccd7e790f1486
Jose Estrada <j.estrada49@icloud.com>
2021-06-17 15:52:48 -0700

    destructuring props in length controls

--- src/components/LengthControls.js
+++ src/components/LengthControls.js
@@ -1,50 +1,58 @@
 import React from 'react'
 import LengthControl from './LengthControl';
 
-export default function LengthControls(props) {
+export default function LengthControls({
+  brkLength,
+  setBrkLength,
+  seshLength,
+  setSeshLength,
+  timerState,
+  timerType,
+  setTimer
+}) {
   const updateBrkLength = (e) => {
-    let currentLength = props.brkLength;
+    let currentLength = brkLength;
     let newTimerType = 'Session';
     let sign = e.currentTarget.dataset.operation;
 
-    if (props.timerState === 'running') {
+    if (timerState === 'running') {
       return;
     }
-    if (props.timerType === newTimerType) {
+    if (timerType === newTimerType) {
       if (sign === '-' && currentLength !== 1) {
-        props.setBrkLength(currentLength - 1);
+        setBrkLength(currentLength - 1);
       } else if (sign === '+' && currentLength !== 60) {
-        props.setBrkLength(currentLength + 1);
+        setBrkLength(currentLength + 1);
       }
     } else if (sign === '-' && currentLength !== 1) {
-      props.setBrkLength(currentLength - 1);
-      props.setTimer(currentLength * 60 - 60);
+      setBrkLength(currentLength - 1);
+      setTimer(currentLength * 60 - 60);
     } else if (sign === '+' && currentLength !== 60) {
-      props.setBrkLength(currentLength + 1);
-      props.setTimer(currentLength * 60 + 60);
+      setBrkLength(currentLength + 1);
+      setTimer(currentLength * 60 + 60);
     }
   }
 
   const updateSeshLength = (e) => {
-    let currentLength = props.seshLength;
+    let currLength = seshLength;
     let newTimerType = 'Break';
     let sign = e.currentTarget.dataset.operation;
 
-    if (props.timerState === 'running') {
+    if (timerState === 'running') {
       return;
     }
-    if (props.timerType === newTimerType) {
-      if (sign === '-' && currentLength !== 1) {
-        props.setSeshLength(currentLength - 1);
-      } else if (sign === '+' && currentLength !== 60) {
-        props.setSeshLength(currentLength + 1);
+    if (timerType === newTimerType) {
+      if (sign === '-' && currLength !== 1) {
+        setSeshLength(currLength - 1);
+      } else if (sign === '+' && currLength !== 60) {
+        setSeshLength(currLength + 1);
       }
-    } else if (sign === '-' && currentLength !== 1) {
-      props.setSeshLength(currentLength - 1);
-      props.setTimer(currentLength * 60 - 60);
-    } else if (sign === '+' && currentLength !== 60) {
-      props.setSeshLength(currentLength + 1);
-      props.setTimer(currentLength * 60 + 60);
+    } else if (sign === '-' && currLength !== 1) {
+      setSeshLength(currLength - 1);
+      setTimer(currLength * 60 - 60);
+    } else if (sign === '+' && currLength !== 60) {
+      setSeshLength(currLength + 1);
+      setTimer(currLength * 60 + 60);
     }
   }
 
@@ -52,7 +60,7 @@ export default function LengthControls(props) {
     <div>
       <LengthControl
         addID = 'session-increment'
-        length = {props.seshLength}
+        length = {seshLength}
         lengthID = 'session-length'
         minID = 'session-decrement'
         onClick={updateSeshLength}
@@ -61,7 +69,7 @@ export default function LengthControls(props) {
       />
       <LengthControl
         addID = 'break-increment'
-        length = {props.brkLength}
+        length = {brkLength}
         lengthID = 'break-length'
         minID = 'break-decrement'
         onClick={updateBrkLength}
