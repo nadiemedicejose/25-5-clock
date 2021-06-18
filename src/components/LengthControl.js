4ba4595c0296af3a428258a0cc6d59e2ed27af23
Jose Estrada <j.estrada49@icloud.com>
2021-06-17 19:05:19 -0700

    simplified length controls props

--- src/components/LengthControl.js
+++ src/components/LengthControl.js
@@ -5,16 +5,20 @@ import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
 const increment = <FontAwesomeIcon icon={faChevronUp} />
 const decrement = <FontAwesomeIcon icon={faChevronDown} />
 
-export default function LengthControl(props) {
+export default function LengthControl({
+  control,
+  title,
+  length,
+  onClick}) {
+
   return (
     <div className='length-control'>
-      <div id={props.titleID}>{props.title}</div>
+      <div id={control + '-label'}>{title}</div>
       <div className='controls-container'>
-        <span id={props.minID} onClick={props.onClick} data-operation='-'>{decrement}</span>
-        <span id={props.lengthID}>{props.length}</span>
-        <span id={props.addID} onClick={props.onClick} data-operation='+'>{increment}</span>
+        <span id={control + '-decrement'} onClick={onClick} data-operation='-'>{decrement}</span>
+        <span id={control + '-length'}>{length}</span>
+        <span id={control + '-increment'} onClick={onClick} data-operation='+'>{increment}</span>
       </div>
     </div>
   )
 }
-
--- src/components/LengthControls.js
+++ src/components/LengthControls.js
@@ -5,10 +5,10 @@ export default function LengthControls({
   state,
   setBrkLength,
   setSeshLength,
-  setTimer
-}) {
+  setTimer }) {
+
   const updateBrkLength = (e) => {
-    let currentLength = state.brkLength;
+    let currLength = state.brkLength;
     let newTimerType = 'Session';
     let sign = e.currentTarget.dataset.operation;
 
@@ -16,17 +16,17 @@ export default function LengthControls({
       return;
     }
     if (state.timerType === newTimerType) {
-      if (sign === '-' && currentLength !== 1) {
-        setBrkLength(currentLength - 1);
-      } else if (sign === '+' && currentLength !== 60) {
-        setBrkLength(currentLength + 1);
+      if (sign === '-' && currLength !== 1) {
+        setBrkLength(currLength - 1);
+      } else if (sign === '+' && currLength !== 60) {
+        setBrkLength(currLength + 1);
       }
-    } else if (sign === '-' && currentLength !== 1) {
-      setBrkLength(currentLength - 1);
-      setTimer(currentLength * 60 - 60);
-    } else if (sign === '+' && currentLength !== 60) {
-      setBrkLength(currentLength + 1);
-      setTimer(currentLength * 60 + 60);
+    } else if (sign === '-' && currLength !== 1) {
+      setBrkLength(currLength - 1);
+      setTimer(currLength * 60 - 60);
+    } else if (sign === '+' && currLength !== 60) {
+      setBrkLength(currLength + 1);
+      setTimer(currLength * 60 + 60);
     }
   }
 
@@ -56,22 +56,16 @@ export default function LengthControls({
   return (
     <div>
       <LengthControl
-        addID = 'session-increment'
+        control = {'session'}
+        title = {'Session Length'}
         length = {state.seshLength}
-        lengthID = 'session-length'
-        minID = 'session-decrement'
-        onClick={updateSeshLength}
-        title = 'Session Length'
-        titleID = 'session-label'
+        onClick = {updateSeshLength}
       />
       <LengthControl
-        addID = 'break-increment'
+        control = {'break'}
+        title = {'Break Length'}
         length = {state.brkLength}
-        lengthID = 'break-length'
-        minID = 'break-decrement'
         onClick={updateBrkLength}
-        title = 'Break Length'
-        titleID = 'break-label'
       />
     </div>
   )
